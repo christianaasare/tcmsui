21637a0ce382c2dcf0f548af8e98c13827f226e6
christianaasare <christiana.asare@turntabl.io>
2019-12-04 14:26:54 +0000

    Removed bootstarp files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,5 +21,11 @@ import { ProjectsComponent } from './projects/projects.component';
   ],
   providers: [],
   bootstrap: [AppComponent]
+})
+
+@NgModule({
+  imports: [
+    HttpClientModule,
+  ],
 })
 export class AppModule { }
